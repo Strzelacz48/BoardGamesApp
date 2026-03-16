eb6c301b0b4f8033fb6d52ab9141102df5179fdf
Marcin Czapulak <73505362+Strzelacz48@users.noreply.github.com>
2026-03-16 08:54:09 +0100

    #2 - Added dashboard and login from Laravel (#8)
    * feat: succesfully set up env with docker desktop
    
    * chore: rebuild from scratch and used lowercase app name
    
    * feat: fixed Redis issue and renamed app to be lowercase
    
    * chore: fixed code formatting for linter
    
    * #1 - improve check pr title workflow
    
    * chore: removed duplicate catalogue
    
    * Update readme.md
    
    Updated for uniformity
    
    Co-authored-by: Ewelina Skrzypacz <56546832+EwelinaSkrzypacz@users.noreply.github.com>
    
    * feat: instalation of starterpack
    
    * feat: login page and dashboard works
    
    * chore: different errors while setting up login screen
    
    * feat: internet domain is working locally
    
    * chore: linter
    
    * #2 - trying to fix css
    
    * #3 - fix running tests
    
    * #3 - trying to fix tests on CI
    
    * #3 - trying to fix tests on CI
    
    * #3 - trying to fix tests on local and CI
    
    * #3 - trying to fix tests on local and CI
    
    * #3 - trying to fix tests on local and CI
    
    * chore: fixed typos in docker-compose with a supervisor
    
    * #2 - trying to fix GitHub Actions warnings
    
    * #2 - removed suggested comments and refactored web.php to separate controllers
    
    * Fixed - PHP tests
    
    * Forgot to run task fix
    
    * #2 - removed comments and addresed ambiguity in validation rules
    
    ---------
    
    Co-authored-by: Strzelacz48 <strzelacz@Strzelacz48-NPC>
    Co-authored-by: EwelinaSkrzypacz <ewelina.skrzypacz@blumilk.pl>
    Co-authored-by: Ewelina Skrzypacz <56546832+EwelinaSkrzypacz@users.noreply.github.com>
    Co-authored-by: Marcin Czapulak <marc.czapulak@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/", fn() => "HELLO WORLD ");
+Route::get("/", [WelcomeController::class, "index"]);
+Route::get("/dashboard", [DashboardController::class, "index"])->middleware(["auth", "verified"])->name("dashboard");
+
+Route::middleware("auth")->group(function (): void {
+    Route::get("/profile", [ProfileController::class, "edit"])->name("profile.edit");
+    Route::patch("/profile", [ProfileController::class, "update"])->name("profile.update");
+    Route::delete("/profile", [ProfileController::class, "destroy"])->name("profile.destroy");
+});
+
+require __DIR__ . "/auth.php";
---
+++ tests/Pest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+uses(TestCase::class, RefreshDatabase::class)->in("Feature");
+
+expect()->extend("toBeOne", fn() => $this->toBe(1));
+
+function something(): void
+{
+}
