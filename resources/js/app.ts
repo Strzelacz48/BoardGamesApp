25946274f0271a03744b6c2883ced5c86a22869f
Marcin Czapulak <marc.czapulak@gmail.com>
2026-07-21 19:08:15 +0200

    - add ZiggyVue with Ziggy config to fix route() function

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -2,6 +2,8 @@ import '../css/app.css'
 import { createApp, h, type DefineComponent } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { ZiggyVue } from '../../vendor/tightenco/ziggy'
+import { Ziggy } from './ziggy'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 
@@ -11,6 +13,7 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .use(ZiggyVue, Ziggy as Parameters<typeof ZiggyVue.install>[1])
       .mount(el)
   },
   progress: {
